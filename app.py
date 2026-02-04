ad6d4c020a434f26f56bf7c8d749ebdfb8acdf19
Anilkumar <akumargc@gmail.com>
2026-02-04 15:35:57 +0530

    Fix DB init for Render (move init_db to main block)

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def init_db():
     conn.commit()
     conn.close()
 
-    init_db()
+    if __name__ == "__main__":
+        init_db()
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"  # required for sessions
